3f8518f96b9e2297ce7fc0f77dea019007374a73
psparkyg <parker.grimes@ufl.edu>
2024-04-10 14:34:34 -0400

    Updated code (decode function included)

--- main.py
+++ main.py
@@ -16,8 +16,28 @@ def encode(old_password):
     new_password = "".join(old_pass_list)
     return new_password
 
-def decode():
-    print()
+def decode(password="12345678"):
+    password_list = []
+    updated_password_list = []
+    decoded_password = ""
+
+    for i in password:
+        password_list.append(i)
+
+    for y in password_list:
+        if y.isdigit() == True:
+            if int(y) == 0:
+                updated_password_list.append("7")
+            elif int(y) == 1:
+                updated_password_list.append("8")
+            elif int(y) == 2:
+                updated_password_list.append("9")
+            else:
+                updated_password_list.append(str(int(y) - 3))
+
+    decoded_password = "".join(updated_password_list)
+
+    return decoded_password
 
 if __name__ == "__main__":
     password = None
@@ -36,7 +56,7 @@ if __name__ == "__main__":
             print("Your password has been encoded and stored!")
         #DECODER
         if menu_option == 2:
-            continue
+            print(f"The encoded password is {password}, and the original password is {decode(password=password)}.")
         if menu_option == 3:
             break
 
